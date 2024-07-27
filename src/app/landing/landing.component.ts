9d0846f3750dd842c0b0e43ce183601c6369849b
Cristian Urbina <urbina1509@gmail.com>
2024-07-27 09:15:32 -0600

    feat: add landing page language selector

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,15 +3,39 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ImageLoaderService } from '../services/image-loader.service';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, CommonModule],
+  imports: [
+    MatIconModule
+    , MatButtonModule
+    , CommonModule
+    , MatSelectModule
+    , MatFormFieldModule
+    , FormsModule
+    , ReactiveFormsModule
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent implements OnInit, OnDestroy {
+  public options = [
+    { value: 'go', label: 'Go', image: 'assets/images/golang-logo.png' },
+    { value: 'rust', label: 'Rust', image: 'assets/images/rust-logo.png' },
+    { value: 'javascript', label: 'Javascript', image: 'assets/images/javascript-logo.png' },
+    { value: 'python', label: 'Python', image: 'assets/images/python-logo.jpg' },
+  ];
+
+  public selectedLanguage: string = 'javascript'
+
+  toppings = new FormControl('');
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
   private imageIndex: number = 0;
   private intervalId: any;
   images: string[] = [];
@@ -26,7 +50,7 @@ export class LandingComponent implements OnInit, OnDestroy {
   startImageRotation(): void {
     this.intervalId = setInterval(() => {
       this.imageIndex = (this.imageIndex + 1) % this.images.length;
-    }, 2000); // Change image every 5 seconds
+    }, 5000);
   }
 
   get currentBackgroundImage(): string {
@@ -37,5 +61,12 @@ export class LandingComponent implements OnInit, OnDestroy {
     if (this.intervalId) {
       clearInterval(this.intervalId);
     }
+  }
+
+  getSelectedOption() {
+    return this.options.find(option => option.value === this.selectedLanguage) || {
+      label: "",
+      image: ""
+    };
   }
 }
